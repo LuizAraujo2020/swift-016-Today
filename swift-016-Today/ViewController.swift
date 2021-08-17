a36a1152c3b4029754e4775de85416d2dc3b61ce
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-17 20:09:48 -0300

    Initial Commit

---
+++ swift-016-Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-016-Today
+//
+//  Created by Luiz Carlos da Silva Araujo on 17/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
