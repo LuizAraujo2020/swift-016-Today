cb08b0150f1e282fb17b3267c81e30807354d8c8
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-18 20:07:12 -0300

    :gift: Configure a Toolbar

--- swift-016-Today/Views/ReminderListCell.swift
+++ swift-016-Today/Views/ReminderListCell.swift
@@ -1,32 +1,27 @@
-//
-//  ReminderListCell.swift
-//  swift-016-Today
-//
-//  Created by Luiz Carlos da Silva Araujo on 17/08/21.
-//
+/*
+ See LICENSE folder for this sample's licensing information.
+ */
 
 import UIKit
 
-
 class ReminderListCell: UITableViewCell {
-  
-  typealias DoneButtonAction = () -> Void //cria um nickname para um tipo
-  //encapsulate later
-  @IBOutlet private var titleLabel: UILabel!
-  @IBOutlet private var dateLabel : UILabel!
-  @IBOutlet private var doneButton: UIButton!
-  
-  private var doneButtonAction: DoneButtonAction?
-  
-  @IBAction func doneButtonTriggered(_ sender: UIButton) {
-    doneButtonAction?()
-  }
-  
-  func configure(title: String, dateText: String, isDone: Bool, doneButtonAction: @escaping DoneButtonAction) {
-    titleLabel.text = title
-    dateLabel.text = dateText
-    let image = isDone ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-    doneButton.setBackgroundImage(image, for: .normal)
-    self.doneButtonAction = doneButtonAction
-  }
+    typealias DoneButtonAction = () -> Void
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+
+    private var doneButtonAction: DoneButtonAction?
+
+    @IBAction func doneButtonTriggered(_ sender: UIButton) {
+        doneButtonAction?()
+    }
+
+    func configure(title: String, dateText: String, isDone: Bool, doneButtonAction: @escaping DoneButtonAction) {
+        titleLabel.text = title
+        dateLabel.text = dateText
+        let image = isDone ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        doneButton.setBackgroundImage(image, for: .normal)
+        self.doneButtonAction = doneButtonAction
+    }
 }
