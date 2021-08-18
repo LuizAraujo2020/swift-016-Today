c83c1743825e50c2cbf2f3e4bdd9f7e4fdd703c2
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-17 23:29:32 -0300

    :gift: Connecting Outlets and Actions

--- swift-016-Today/ReminderListCell.swift
+++ swift-016-Today/ReminderListCell.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
+
 class ReminderListCell: UITableViewCell {
+  
+  typealias DoneButtonAction = () -> Void //cria um nickname para um tipo
   
   @IBOutlet var titleLabel: UILabel!
   @IBOutlet var dateLabel : UILabel!
   @IBOutlet var doneButton: UIButton!
   
+  var doneButtonAction: DoneButtonAction?
+  
   @IBAction func doneButtonTriggered(_ sender: UIButton) {
-//    doneButtonAction?()
+    
+    doneButtonAction?()
+    
   }
   
 }
