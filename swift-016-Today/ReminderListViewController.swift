9b79e76c8ebe9434519a8c584ce38b085d60547a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-18 20:41:46 -0300

    :gift: Configure a Toolbar

--- swift-016-Today/ReminderListViewController.swift
+++ swift-016-Today/ReminderListViewController.swift
@@ -1,33 +1,48 @@
-/*
- See LICENSE folder for this sample's licensing information.
- */
+//
+//  ViewController.swift
+//  swift-016-Today
+//
+//  Created by Luiz Carlos da Silva Araujo on 17/08/21.
+//
 
 import UIKit
 
 class ReminderListViewController: UITableViewController {
-    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
-
-    private var reminderListDataSource: ReminderListDataSource?
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == Self.showDetailSegueIdentifier,
-            let destination = segue.destination as? ReminderDetailViewController,
-            let cell = sender as? UITableViewCell,
-            let indexPath = tableView.indexPath(for: cell) {
-            let rowIndex = indexPath.row
-            guard let reminder = reminderListDataSource?.reminder(at: rowIndex) else {
-                fatalError("Couldn't find data source for reminder list.")
-            }
-            destination.configure(with: reminder) { reminder in
-                self.reminderListDataSource?.update(reminder, at: rowIndex)
-                self.tableView.reloadRows(at: [indexPath], with: .automatic)
-            }
-        }
+  static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+  
+  private var reminderListDataSource: ReminderListDataSource?
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == Self.showDetailSegueIdentifier,
+       let destination = segue.destination as? ReminderDetailViewController,
+       let cell = sender as? UITableViewCell,
+       let indexPath = tableView.indexPath(for: cell) {
+      let rowIndex = indexPath.row
+      guard let reminder = reminderListDataSource?.reminder(at: rowIndex) else {
+        fatalError("Couldn't find data source for reminder list.")
+      }
+      destination.configure(with: reminder) { reminder in
+        self.reminderListDataSource?.update(reminder, at: rowIndex)
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+      }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        reminderListDataSource = ReminderListDataSource()
-        tableView.dataSource = reminderListDataSource
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    reminderListDataSource = ReminderListDataSource()
+    tableView.dataSource = reminderListDataSource
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    if let navigationController = navigationController,
+       navigationController.isToolbarHidden {
+      navigationController.setToolbarHidden(false, animated: animated)
     }
+  }
+  
+  @IBAction func addButtonTriggered(_ sender: UIBarButtonItem) {
+  }
+  
 }
